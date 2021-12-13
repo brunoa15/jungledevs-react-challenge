b45206faba36c30eb2e097fbe63a05624267b547
Bruno Vieira <bruno.antonio.vieira@icloud.com>
2021-12-13 15:09:19 -0300

    all sections sctructures done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,9 @@ import playButton from '../../assets/images/play-button.svg'
 import manageNannyImage from '../../assets/images/manage-nanny-image.png'
 import roundedProfileImage from '../../assets/images/rounded-profile-image.png'
 import imageSection1 from '../../assets/images/image-section-1.png'
+import imageSection3 from '../../assets/images/image-section-3.png'
+import imageSection4 from '../../assets/images/image-section-4.png'
+import imageSection5 from '../../assets/images/image-section-5.png'
 
 const Home = () => {
   const [state, setState] = useState()
@@ -47,14 +50,12 @@ const Home = () => {
           </div>
         </section>
         <div className="lp-body__main-container">
-          <section className="lp-body__section-1">
+          <section className="lp-body__section lp-body__section--mobile">
             <div className="lp-body__section-img">
               <img
                 loading="lazy"
                 src={imageSection1}
                 alt="share your home, nanny and costs"
-                width="auto"
-                height="90%"
               />
             </div>
             <h2 className="lp-body__section-title lp-body__section-title--narrow">
@@ -74,6 +75,99 @@ const Home = () => {
             </div>
             <div className="lp-body__section-link">
               <a className="lp-link lp-link--purple" href="/">Ready to get started?</a>
+            </div>
+          </section>
+          <div className="lp-body__line-container">
+            <div className="common__vertical-line common__vertical-line--margin-80 common__vertical-line--no-margin-mobile" />
+          </div>
+          <section className="lp-body__section-inputs">
+            <h3 className="lp-body__section-title lp-body__section-title--inputs">
+              Are you a parent without a nanny and looking to share?
+            </h3>
+            <div className="lp-body__section-text lp-body__section-text--inputs">
+              <p>
+                Leave us your name and email and we'll update you as soon as a share becomes
+                available in your area!
+              </p>
+            </div>
+          </section>
+          <div className="lp-body__line-container">
+            <div className="common__vertical-line common__vertical-line--margin-80 common__vertical-line--no-margin-mobile" />
+          </div>
+          <section className="lp-body__section lp-body__section--left-image lp-body__section--mobile">
+            <div className="lp-body__section-img">
+              <img
+                loading="lazy"
+                src={imageSection3}
+                alt="Shared payments made simple"
+              />
+            </div>
+            <h2 className="lp-body__section-title lp-body__section-title--mobile-narrow">
+              Shared payments made simple
+            </h2>
+            <div className="lp-body__section-text">
+              <p>
+                Sometimes it's hard enough just sharing a restaurant bill. Try sharing that
+                bill week in, week out and you might encounter more than a few snares. But
+                not with Hapu. Simply set your rates and our automated payment system takes
+                care of the rest. You need never talk money or who owes what.
+              </p>
+            </div>
+            <div className="lp-body__section-link">
+              <a className="lp-link lp-link--purple" href="/">
+                Read how Bridget's share (without Hapu) ended over $15
+              </a>
+            </div>
+          </section>
+          <div className="lp-body__line-container">
+            <div className="common__vertical-line common__vertical-line--margin-80 common__vertical-line--no-margin-mobile" />
+          </div>
+          <section className="lp-body__section-column">
+            <div className="lp-body__section-img lp-body__section-img--no-mobile-img">
+              <img
+                loading="lazy"
+                src={imageSection4}
+                alt="A framework built for the long term"
+              />
+            </div>
+            <h2 className="lp-body__section-title lp-body__section-title--mobile-narrow">
+              A framework built for the long term
+            </h2>
+            <div className="lp-body__section-text">
+              <p>
+                Childcare is for the long term. And you need a framework you can count on 
+                that gives your share long term viability and success. That's why we've 
+                defined Hapu around our three tribal principles; clearly defined process,
+                transparency over money and equality of participation.
+              </p>
+            </div>
+            <div className="lp-body__section-link">
+              <a className="lp-link lp-link--purple" href="/">
+                Read how Hapu's tribal background defines our app 
+              </a>
+            </div>
+          </section>
+          <div className="lp-body__line-container">
+            <div className="common__vertical-line common__vertical-line--margin-80 common__vertical-line--no-margin-mobile" />
+          </div>
+          <section className="lp-body__section-column lp-body__section-column--no-link">
+            <div className="lp-body__section-img">
+              <img
+                loading="lazy"
+                src={imageSection5}
+                alt="A framework built for the long term"
+              />
+            </div>
+            <h2 className="lp-body__section-title lp-body__section-title--mobile-narrow">
+              Coming soon: Nanny Share Daily Diary!
+            </h2>
+            <div className="lp-body__section-text">
+              <p>
+                With the Hapu daily diary your nanny will be able to update you and your
+                sharers with photos and commentary of the day. You and sharers will receive
+                notifications and you'll be able to login to view the daily adventures fo
+                your little ones. We can't wait!
+              </p>
             </div>
           </section>
         </div>
